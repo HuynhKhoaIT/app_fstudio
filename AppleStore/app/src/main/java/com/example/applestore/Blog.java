e95a688882f5cf9bbc2d325afe36c6fdb05adb8a
HuynhKhoaIT <114349590+HuynhKhoaIT@users.noreply.github.com>
2023-04-16 22:41:41 +0700

    bug ở detail blog

---
+++ AppleStore/app/src/main/java/com/example/applestore/Blog.java
@@ -0,0 +1,39 @@
+package com.example.applestore;
+
+public class Blog {
+    private int image;
+    private String name;
+
+    private String desc;
+
+
+    public Blog(int image, String name, String desc) {
+        this.image = image;
+        this.name = name;
+        this.desc = desc;
+    }
+
+
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {this.desc = desc; }
+}
