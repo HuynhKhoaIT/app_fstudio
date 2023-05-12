7fa7bbd952450c2645ad48ec5123de9a28e1a21d
HuynhKhoaIT <114349590+HuynhKhoaIT@users.noreply.github.com>
2023-05-12 10:51:03 +0700

    hiển thị list danh mục nhưng chưa làm xong

---
+++ AppleStore/app/src/main/java/com/example/applestore/Activity/DetailCategoryActivity.java
@@ -0,0 +1,18 @@
+package com.example.applestore.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.applestore.R;
+
+public class DetailCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_category);
+
+
+    }
+}
