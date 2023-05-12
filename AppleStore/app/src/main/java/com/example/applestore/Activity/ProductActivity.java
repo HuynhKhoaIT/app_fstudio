bc3c8c6500b082117d16f2aadce63695a2ee274a
HuynhKhoaIT <114349590+HuynhKhoaIT@users.noreply.github.com>
2023-05-12 16:11:35 +0700

    thêm api trong ProductActivity để hiện thi list product theo category

--- AppleStore/app/src/main/java/com/example/applestore/Activity/ProductActivity.java
+++ AppleStore/app/src/main/java/com/example/applestore/Activity/ProductActivity.java
@@ -47,71 +47,72 @@ public class ProductActivity extends AppCompatActivity {
         Intent intent = getIntent();
         id = getIntent().getIntExtra(CategoryAdapter.KEY_CATEGORYID_TO_PRODUCT, 0);
     }
-    private void getProducts(){
-
-        Call<List<Product>> call = apiService.getProduct(id);
-        call.enqueue(new Callback<List<Product>>() {
-                         @Override
-                         public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
-
-                             if (response.isSuccessful()) {
-                                 productList = response.body();
-
-                                 ProductAdapter productAdapter = new ProductAdapter(context, R.layout.row_item_product, productList);
-
-                                 gvProduct.setAdapter(productAdapter);
-
-                                 gvProduct.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                                     @Override
-                                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                         Intent intent = new Intent(context, ProductDetailActivity.class);
-                                         intent.putExtra(KEY_PRODUCT_ID_TO_PRODUCT_DETAIL, position);
-                                         startActivity(intent);
-                                     }
-                                 });
-
-                             }
-                             else {
-                                 Toast.makeText(ProductActivity.this, "fail", Toast.LENGTH_LONG).show();
-                             }
-}
-
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Product>> call, Throwable t) {
-//                Log.i("fail ", t.getMessage());
-//            }
-//        });
-//}
 //    private void getProducts(){
-//        Call<List<Product>> call = apiService.getAllProduct();
+//
+//        Call<List<Product>> call = apiService.getProduct(id);
 //        call.enqueue(new Callback<List<Product>>() {
-//            @Override
-//            public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
+//                         @Override
+//                         public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
 //
-//                if(response.isSuccessful()){
-//                    System.out.println(response.body().toString());
-//                    productList = response.body();
-//                    System.out.println(response.body());
-//                    System.out.println("Zoo");
-//                    System.out.println(productList.get(0).getAnh());
-//                    productAdapter = new ProductAdapter(getContext(),productList);
-//                    mRecyclerView.setHasFixedSize(true);
-//                    mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
-//                    mRecyclerView.setAdapter(productAdapter);
-//                    productAdapter.notifyDataSetChanged();
+//                             if (response.isSuccessful()) {
+//                                 productList = response.body();
 //
-//                }else{
-//                    Log.i("TAG","fail");
-//                    System.out.println("Zoo - Errors");
-//                }
-//            }
-//            @Override
-//            public void onFailure(Call<List<Product>> call, Throwable t) {
-//                Log.i("TAG", t.toString());
-//                System.out.println("Zoo - Errors");
-//            }
-//        });
-//    }
+//                                 ProductAdapter productAdapter = new ProductAdapter(context, R.layout.row_item_product, productList);
+//
+//                                 gvProduct.setAdapter(productAdapter);
+//
+//                                 gvProduct.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                                     @Override
+//                                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                                         Intent intent = new Intent(context, ProductDetailActivity.class);
+//                                         intent.putExtra(KEY_PRODUCT_ID_TO_PRODUCT_DETAIL, position);
+//                                         startActivity(intent);
+//                                     }
+//                                 });
+//
+//                             }
+//                             else {
+//                                 Toast.makeText(ProductActivity.this, "fail", Toast.LENGTH_LONG).show();
+//                             }
+//}
+//
+////
+////            }
+////
+////            @Override
+////            public void onFailure(Call<List<Product>> call, Throwable t) {
+////                Log.i("fail ", t.getMessage());
+////            }
+////        });
+////}
+////    private void getProducts(){
+////        Call<List<Product>> call = apiService.getAllProduct();
+////        call.enqueue(new Callback<List<Product>>() {
+////            @Override
+////            public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
+////
+////                if(response.isSuccessful()){
+////                    System.out.println(response.body().toString());
+////                    productList = response.body();
+////                    System.out.println(response.body());
+////                    System.out.println("Zoo");
+////                    System.out.println(productList.get(0).getAnh());
+////                    productAdapter = new ProductAdapter(getContext(),productList);
+////                    mRecyclerView.setHasFixedSize(true);
+////                    mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+////                    mRecyclerView.setAdapter(productAdapter);
+////                    productAdapter.notifyDataSetChanged();
+////
+////                }else{
+////                    Log.i("TAG","fail");
+////                    System.out.println("Zoo - Errors");
+////                }
+////            }
+////            @Override
+////            public void onFailure(Call<List<Product>> call, Throwable t) {
+////                Log.i("TAG", t.toString());
+////                System.out.println("Zoo - Errors");
+////            }
+////        });
+////    }
+}
